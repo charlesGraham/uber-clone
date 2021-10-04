d65facb13738238f4dd3bb04b8f9c96be21aa650
Charles <crg0905@gmail.com>
2021-10-04 14:28:52 -0700

    slices

--- store.js
+++ store.js
@@ -1,7 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import navReducer from "./slices/navSlice";
+import { configureStore } from "@reduxjs/toolkit"; //configures data layer 
+import navReducer from "./slices/navSlice"; // "slice" of the data layer
 
 export const store = configureStore({
+    // setup global data layer
     reducer: {
         nav: navReducer,
     },
