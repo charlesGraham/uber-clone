a8f57c7ab5961ef2662681614f84e53671a845f8
Charles <crg0905@gmail.com>
2021-10-04 13:02:52 -0700

    install redux tools

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// TODO: setup redux
+// TODO: 
+
 export default function App() {
   return (
     <View style={styles.container}>
