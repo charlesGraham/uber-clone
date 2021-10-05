0e86f03a1d1d046b1a092f868cc926b51365d20b
Charles <crg0905@gmail.com>
2021-10-05 09:36:23 -0700

    build home screen component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
-            <Text style={tw`text-red-500 p-10`} >this is the homescreen</Text>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain',
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs",
+                    }}
+                />
+            </View>
         </SafeAreaView>
     )
 }
