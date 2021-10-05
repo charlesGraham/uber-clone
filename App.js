cdb76f22895f896b5a3a89c92dbc795010bfae6d
Charles <crg0905@gmail.com>
2021-10-05 09:57:39 -0700

    wrap app in SafeAreaProvider

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 // TODO: 
@@ -11,7 +12,9 @@ import { store } from './store';
 export default function App() {
   return (
     <Provider store={store}>
-        <HomeScreen />
+      <SafeAreaProvider>
+      <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
     
   );
