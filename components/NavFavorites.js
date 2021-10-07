288c0f4a1f926b6396508cda600870d8b76c03be
Charles <crg0905@gmail.com>
2021-10-06 22:07:09 -0700

    icons and styles for NavFavorites

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from "react-native-elements";
+import tw from 'tailwind-react-native-classnames';
 
 const data = [
     {
@@ -21,9 +23,20 @@ const NavFavorites = () => {
         <FlatList
             data={data}
             keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-                <TouchableOpacity>
-                    <Text>wsup</Text>
+            ItemSeparatorComponent={() => <View style={tw`bg-gray-200 h-1`} />}
+            renderItem={({ item: { location, destination, icon } }) => (
+                <TouchableOpacity style={tw`flex-row items-center p-5`}>
+                    <Icon
+                        style={tw` mr-4 rounded-full bg-gray-300 p-3`}
+                        name={icon}
+                        type="ionicon"
+                        color="white"
+                        size={18}
+                    />
+                    <View>
+                        <Text style={tw`font-semibold text-lg`}>{location}</Text>
+                        <Text style={tw`text-gray-500`}>{destination}</Text>
+                    </View>
                 </TouchableOpacity>
             )}
         />
